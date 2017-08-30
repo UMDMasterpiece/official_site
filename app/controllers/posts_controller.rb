221e8f2d56e6f5f127575db7b35021f2b407fcdb
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-08-30 12:10:45 -0400

    designing blog show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
     private
   
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :description, :banner_image_url)
     end
   
     def find_post 
